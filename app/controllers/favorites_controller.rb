6fd9e696656b8498fff4949f21507030a0897115
hagaryota <ryota_haga@mediado.jp>
2022-04-15 16:38:09 +0900

    test

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
-
   def create
     current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
   end
 
   def destroy
     current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
   end
 
+  private
+
+  def blog_params
+    @blog = Blog.find(params[:id])
+  end
 end
